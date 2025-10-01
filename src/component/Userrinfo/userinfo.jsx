3ba812a87cdd88b05353da0a3acfef680a91e3fc
Jin <kbg08167@naver.com>
2025-10-01 16:58:12 +0900

    profile 반응형

--- src/component/Userrinfo/userinfo.jsx
+++ src/component/Userrinfo/userinfo.jsx
@@ -16,6 +16,16 @@ const ChangePwPage = () => {
 
 function UserInfo() {
   const avatar = useMyAvatar();
+  const navigate = useNavigate();
+  const [myData, setMyData] = useState({
+    full_name: "",
+    email: "",
+    registered_at: "",
+    diary_count: "",
+  });
+  const [myDiaryOn, setMyDiaryOn] = useState(false);
+  const [myInfoOn, setMyInfoOn] = useState(false);
+
   useEffect(() => {
     if (!avatar) return;
     const responseData = async () => {
@@ -31,15 +41,6 @@ function UserInfo() {
     };
     responseData();
   }, [avatar]);
-  const navigate = useNavigate();
-  const [myData, setMyData] = useState({
-    full_name: "",
-    email: "",
-    registered_at: "",
-    diary_count: "",
-  });
-  const [myDiaryOn, setMyDiaryOn] = useState(false);
-  const [myInfoOn, setMyInfoOn] = useState(false);
   const updateSetting = async (field, value) => {
     try {
       await api.patch(
@@ -55,11 +56,6 @@ function UserInfo() {
     }
   };
 
-  const handleOnClick = () => {
-    const newValue = !myDiaryOn;
-    setMyDiaryOn(newValue);
-    updateSetting("hide_diaries", newValue);
-  };
   useEffect(() => {
     const getUserData = async () => {
       try {
@@ -80,6 +76,32 @@ function UserInfo() {
     };
     getUserData();
   }, []);
+
+  useEffect(() => {
+    const ChangePW = async (msg) => {
+      if (msg.data === "ChangePW") {
+        console.log("success get msg");
+        try {
+          const res = await api.post("/auth/logout");
+          console.log("success logout", res.data);
+        } catch (err) {
+          console.log("Change PW Message err", err.response);
+        }
+        alert("비밀번호가 변경되었습니다. 다시 로그인 해주세요.");
+        navigate("/login");
+      }
+    };
+    window.addEventListener("message", ChangePW);
+    return () => {
+      window.removeEventListener("message", ChangePW);
+    };
+  }, [navigate]);
+
+  const handleOnClick = () => {
+    const newValue = !myDiaryOn;
+    setMyDiaryOn(newValue);
+    updateSetting("hide_diaries", newValue);
+  };
 
   const handleOnClick2 = () => {
     const newValue = !myInfoOn;
